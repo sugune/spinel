e2c3d1b9c9d0d660cd9ed7ef130cd0b48bc4c3a9
cyrus <cymine2404@gmail.com>
2022-11-04 08:10:53 +0800

    changes

--- mech.js
+++ mech.js
@@ -1,7 +1,10 @@
 const menu = document.querySelector('.nav-menu');
 const navBtn = document.querySelector('.nav-btn');
 const navbar = document.querySelector('.navbar');
+const links = [...document.querySelectorAll('.nav-link'), document.querySelector('.watch')];
 
+
+// navbar functionality
 
 navBtn.addEventListener('click', () => {
   menu.classList.toggle('show-menu');
@@ -11,8 +14,23 @@ navBtn.addEventListener('click', () => {
 window.addEventListener('scroll', () => {
   const navHeight = navbar.getBoundingClientRect().height
   if (window.pageYOffset > navHeight) {
-    navbar.classList.add('fixed-nav')
+    navbar.classList.add('fixed-nav');
   } else {
     navbar.classList.remove('fixed-nav');
   }
 })
+
+links.forEach(link => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    const data = link.getAttribute('href');
+    const element = document.querySelector(data);
+    const navHeight = navbar.getBoundingClientRect().height
+    const destination = element.offsetTop;
+    window.scroll({
+      top: destination - navHeight
+    });
+    menu.classList.remove('show-menu');
+    navBtn.classList.remove('is-active');
+  });
+});
